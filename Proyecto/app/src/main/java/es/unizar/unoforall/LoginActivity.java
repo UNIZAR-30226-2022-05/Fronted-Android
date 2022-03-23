4f0614b693faa3f0d00adaf2c49dc925d9b831cc
Jorge <777570@unizar.es>
2022-03-23 12:32:31 +0100

    Añadida api rest al login y registro de usuarios

--- Proyecto/app/src/main/java/es/unizar/unoforall/LoginActivity.java
+++ Proyecto/app/src/main/java/es/unizar/unoforall/LoginActivity.java
@@ -1,5 +1,7 @@
 package es.unizar.unoforall;
 
+import static es.unizar.unoforall.utils.HashUtils.cifrarContrasenna;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
@@ -7,11 +9,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import es.unizar.unoforall.api.RestAPI;
+import es.unizar.unoforall.database.UsuarioDbAdapter;
+
+import java.util.UUID;
 
 public class LoginActivity extends AppCompatActivity{
 
     private EditText mailText;
     private EditText passwordText;
+    private UsuarioDbAdapter mDbHelper;
+    private Long mRowId;
+
+    private class RespuestaLogin {
+        public boolean exito;
+        public String errorInfo;
+        public UUID sesionID;
+
+        public RespuestaLogin(boolean exito, String errorInfo, UUID sessionID) {
+            this.exito = exito;
+            this.errorInfo = errorInfo;
+            this.sesionID = sessionID;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +41,36 @@ public class LoginActivity extends AppCompatActivity{
         setContentView(R.layout.login);
         setTitle(R.string.login);
 
+        mDbHelper = new UsuarioDbAdapter(this);
+        mDbHelper.open();
+
         mailText = (EditText) findViewById(R.id.correo);
         passwordText = (EditText) findViewById(R.id.contrasenna);
+
+        String mail = mailText.getText().toString();
+        String contrasenna = passwordText.getText().toString();
+
+        String contrasennaHash = cifrarContrasenna(contrasenna);
 
         Button confirmLogin = (Button) findViewById(R.id.login);
 
         confirmLogin.setOnClickListener(view -> {
             setResult(RESULT_OK);
             //envio de los datos al servidor
+            RestAPI api = new RestAPI("/api/login");
+            api.addParameter("Valor1", mail);
+            api.addParameter("Valor2", contrasennaHash);
+            api.openConnection();
 
             //recepcion de los datos y actuar en consecuencia
+            RespuestaLogin resp = api.receiveObject(RespuestaLogin.class);
+            if(resp.exito){
+                mRowId = mDbHelper.createUsuario(mail, contrasennaHash);
+                //satamos a la pantalla de menu principal del juego
+            } else {
+                Toast.makeText(LoginActivity.this, resp.errorInfo, Toast.LENGTH_SHORT).show();
+                return;
+            }
             //finish();
         });
     }
--- Proyecto/app/src/main/java/es/unizar/unoforall/RegisterActivity.java
+++ Proyecto/app/src/main/java/es/unizar/unoforall/RegisterActivity.java
@@ -2,12 +2,16 @@ package es.unizar.unoforall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import static es.unizar.unoforall.utils.HashUtils.cifrarContrasenna;
+
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import es.unizar.unoforall.api.RestAPI;
 
 
 public class RegisterActivity extends AppCompatActivity{
@@ -28,6 +32,12 @@ public class RegisterActivity extends AppCompatActivity{
         passwordText = (EditText) findViewById(R.id.contrasenna);
         passBisText = (EditText) findViewById(R.id.contrasennabis);
 
+        String userName = userNameText.getText().toString();
+        String mail = mailText.getText().toString();
+
+        String contrasenna = passwordText.getText().toString();
+        String contrasennaHash = cifrarContrasenna(contrasenna);
+
         Button confirmRegister = (Button) findViewById(R.id.register);
 
         confirmRegister.setOnClickListener(view -> {
@@ -40,7 +50,20 @@ public class RegisterActivity extends AppCompatActivity{
             }
             //envio de los datos al servidor
 
+            RestAPI api = new RestAPI("/api/registerStepOne");
+            api.addParameter("Valor1", mail);
+            api.addParameter("Valor2", contrasennaHash);
+            api.addParameter("Valor3", userName);
+            api.openConnection();
             //recepcion de los datos y actuar en consecuencia
+
+            String resp = api.receiveObject(String.class);
+            if (resp.equals(null)){
+                //Usuario registrado y cambiamos a la pantalla de inicio
+            } else {
+                Toast.makeText(RegisterActivity.this, resp, Toast.LENGTH_SHORT).show();
+                return;
+            }
             //finish();
         });
     }
