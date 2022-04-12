3097331e6a0abfc3b2947c8ad7b4dad07dedad80
Jorge <777570@unizar.es>
2022-04-12 13:01:04 +0200

    Comienzo de la gestión de notificaciones

--- Proyecto/app/src/main/java/es/unizar/unoforall/InicioActivity.java
+++ Proyecto/app/src/main/java/es/unizar/unoforall/InicioActivity.java
@@ -3,7 +3,10 @@ package es.unizar.unoforall;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,12 +20,15 @@ import es.unizar.unoforall.utils.dialogs.SetIPDialogBuilder;
 public class InicioActivity extends AppCompatActivity {
 
     private static final int CAMBIAR_IP_ID = 0;
+    private static final String CHANNEL_ID = NotificationChannel.DEFAULT_CHANNEL_ID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
         setTitle(R.string.app_name);
+
+        createNotificationChannel();
 
         Button botonRegistro = (Button)findViewById(R.id.botonRegistro);
         Button botonLogin = (Button)findViewById(R.id.botonLogin);
@@ -64,5 +70,25 @@ public class InicioActivity extends AppCompatActivity {
            dialog.dismiss();
         });
         builder.create().show();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channelName);
+            String description = getString(R.string.channelDescription);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public static String getChannelId() {
+        return CHANNEL_ID;
     }
 }
