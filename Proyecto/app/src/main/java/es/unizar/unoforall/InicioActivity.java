a1b92afd9732e5c0381be0c3823dcab7391160cb
ivanp23 <ivanpla2001@gmail.com>
2022-04-23 20:15:31 +0200

    Añadida opción para desactivar el modo de optimización de batería
    De esta forma, al dejar el móvil en modo de suspensión (pantalla apagada), ya no se cerrará la sesión de forma inesperada

--- Proyecto/app/src/main/java/es/unizar/unoforall/InicioActivity.java
+++ Proyecto/app/src/main/java/es/unizar/unoforall/InicioActivity.java
@@ -1,9 +1,14 @@
 package es.unizar.unoforall;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
@@ -20,6 +25,9 @@ import es.unizar.unoforall.utils.ActivityType;
 public class InicioActivity extends CustomActivity {
 
     private static final int CAMBIAR_IP_ID = 0;
+    private static final int REQUEST_DISABLE_BATTERY_OPTIMIZATION_INTENT = 123;
+    private static final int DISABLE_OK = -1;
+    private static final int DISABLE_FAILED = 0;
 
     @Override
     public ActivityType getType(){
@@ -39,6 +47,50 @@ public class InicioActivity extends CustomActivity {
         botonLogin.setOnClickListener(v->startActivityForResult(new Intent(InicioActivity.this, LoginActivity.class), 0));
 
         NotificationManager.initialize(this);
+
+        requestDisableBatteryOptimization();
+    }
+
+    private void requestDisableBatteryOptimization(){
+        String pkg=getPackageName();
+        PowerManager pm=getSystemService(PowerManager.class);
+        if (pm.isIgnoringBatteryOptimizations(pkg)) {
+            return;
+        }
+
+        if (Build.VERSION.SDK_INT>Build.VERSION_CODES.LOLLIPOP_MR1) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Desactivar optimización de batería");
+            builder.setMessage("UnoForAll requiere desactivar el modo " +
+                    "de optimización de batería para funcionar correctamente." +
+                    "\n" +
+                    "¿Deseas desactivarlo?");
+            builder.setPositiveButton("Sí", (dialog, which) -> {
+                Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
+                                .setData(Uri.parse("package:" + pkg));
+                startActivityForResult(intent, REQUEST_DISABLE_BATTERY_OPTIMIZATION_INTENT);
+            });
+            builder.setNegativeButton("No, salir de la aplicación", (dialog, which) -> {
+                finish();
+            });
+            builder.setOnCancelListener(dialog -> builder.show());
+            builder.show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_DISABLE_BATTERY_OPTIMIZATION_INTENT){
+            switch(resultCode){
+                case DISABLE_OK:
+                    mostrarMensaje("Modo de optimización de batería desactivado");
+                    break;
+                case DISABLE_FAILED:
+                    requestDisableBatteryOptimization();
+                    break;
+            }
+        }
     }
 
     @Override
