43fbdd0d2a21ba2857e3ccc83585ada73744b751
Jorge <777570@unizar.es>
2022-04-23 13:00:52 +0200

    Creadas funciones del backend para la personalizacion

--- Proyecto/app/src/main/java/es/unizar/unoforall/utils/dialogs/ModifyAspectDialogBuilder.java
+++ Proyecto/app/src/main/java/es/unizar/unoforall/utils/dialogs/ModifyAspectDialogBuilder.java
@@ -1,5 +1,20 @@
 package es.unizar.unoforall.utils.dialogs;
 
+import android.app.Activity;
+
+import es.unizar.unoforall.model.UsuarioVO;
+
 public class ModifyAspectDialogBuilder {
+
+    private final Activity activity;
+    private UsuarioVO usuarioVO;
+
+    private Runnable positiveRunnable;
+    private Runnable negativeRunnable;
+
+    public ModifyAspectDialogBuilder(Activity activity, UsuarioVO usuarioVO) {
+        this.activity = activity;
+        this.usuarioVO = usuarioVO;
+    }
 
 }
