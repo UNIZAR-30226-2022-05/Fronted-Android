27238ddd6218c0817f1328cd2613910471201aba
Iván <ivanpla2001@gmail.com>
2022-05-03 10:57:20 +0200

    Añadido visualización del modo por parejas en la pantalla de resultados

--- Proyecto/app/src/main/java/es/unizar/unoforall/utils/dialogs/MostrarResultadosDialogBuilder.java
+++ Proyecto/app/src/main/java/es/unizar/unoforall/utils/dialogs/MostrarResultadosDialogBuilder.java
@@ -10,18 +10,12 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 
-import org.w3c.dom.Text;
-
-import java.util.Comparator;
-
 import es.unizar.unoforall.PartidaActivity;
 import es.unizar.unoforall.R;
-import es.unizar.unoforall.api.BackendAPI;
-import es.unizar.unoforall.api.Serializar;
 import es.unizar.unoforall.model.UsuarioVO;
-import es.unizar.unoforall.model.partidas.Jugada;
 import es.unizar.unoforall.model.partidas.Participante;
 import es.unizar.unoforall.model.partidas.PartidaJugada;
+import es.unizar.unoforall.model.salas.ConfigSala;
 import es.unizar.unoforall.model.salas.Sala;
 import es.unizar.unoforall.utils.CustomActivity;
 import es.unizar.unoforall.utils.ImageManager;
@@ -30,6 +24,11 @@ public class MostrarResultadosDialogBuilder {
     private final CustomActivity activity;
 
     private View mainView;
+
+    private TextView[] textViewsNumerosPuestos;
+    private TextView[] textViewsPuestos;
+    private TextView[] textViewsPuntos;
+    private ImageView[] imageViewsPuestos;
 
     private Runnable positiveRunnable = () -> {};
     private Runnable negativeRunnable = () -> {};
@@ -61,48 +60,58 @@ public class MostrarResultadosDialogBuilder {
                 break;
         }
 
-        TextView[] textViewsPuestos = new TextView[] {
+        textViewsNumerosPuestos = new TextView[] {
+                this.mainView.findViewById(R.id.numeroPuesto0),
+                this.mainView.findViewById(R.id.numeroPuesto1),
+                this.mainView.findViewById(R.id.numeroPuesto2),
+                this.mainView.findViewById(R.id.numeroPuesto3)
+        };
+        textViewsPuestos = new TextView[] {
                 this.mainView.findViewById(R.id.textViewPuesto0),
                 this.mainView.findViewById(R.id.textViewPuesto1),
                 this.mainView.findViewById(R.id.textViewPuesto2),
                 this.mainView.findViewById(R.id.textViewPuesto3),
         };
-        TextView[] textViewsPuntos = new TextView[] {
+        textViewsPuntos = new TextView[] {
                 this.mainView.findViewById(R.id.textViewPuntos0),
                 this.mainView.findViewById(R.id.textViewPuntos1),
                 this.mainView.findViewById(R.id.textViewPuntos2),
                 this.mainView.findViewById(R.id.textViewPuntos3)
         };
-        ImageView[] imageViewsPuestos = new ImageView[] {
+        imageViewsPuestos = new ImageView[] {
                 this.mainView.findViewById(R.id.imageViewPuesto0),
                 this.mainView.findViewById(R.id.imageViewPuesto1),
                 this.mainView.findViewById(R.id.imageViewPuesto2),
                 this.mainView.findViewById(R.id.imageViewPuesto3)
         };
 
+        boolean modoPorParejas = sala.getConfiguracion().getModoJuego() == ConfigSala.ModoJuego.Parejas;
+        if(modoPorParejas){
+            textViewsNumerosPuestos[0].setText("1º");textViewsNumerosPuestos[0].setTextColor(activity.getColor(R.color.color_primer_puesto));
+            textViewsNumerosPuestos[1].setText("1º");textViewsNumerosPuestos[1].setTextColor(activity.getColor(R.color.color_primer_puesto));
+            textViewsNumerosPuestos[2].setText("2º");textViewsNumerosPuestos[2].setTextColor(activity.getColor(R.color.color_segundo_puesto));
+            textViewsNumerosPuestos[3].setText("2º");textViewsNumerosPuestos[3].setTextColor(activity.getColor(R.color.color_segundo_puesto));
+        }
+
         for(int i=0; i<partidaJugada.getParticipantes().size(); i++){
             Participante participante = partidaJugada.getParticipantes().get(i);
             UsuarioVO usuario = participante.getUsuario();
             int puntos = participante.getPuntos();
             int puesto = participante.getPuesto();
-            String nombre;
-            int imageID;
-            if(usuario == null){
-                // Es una IA
-                nombre = PartidaActivity.getIAName();
-                imageID = ImageManager.IA_IMAGE_ID;
-            }else{
-                nombre = usuario.getNombre();
-                imageID = usuario.getAvatar();
-            }
 
-            ImageManager.setImagenPerfil(imageViewsPuestos[puesto-1], imageID);
-            textViewsPuestos[puesto-1].setText(nombre);
-            if(usuario != null){
-                textViewsPuntos[puesto-1].setText(puntos + " puntos");
-            }else{
-                textViewsPuntos[puesto-1].setVisibility(View.GONE);
-            }
+            setDatosUsuario(puesto, puntos, usuario);
+        }
+    }
+
+    private void setDatosUsuario(int puesto, int puntos, UsuarioVO usuario){
+        if(usuario != null){
+            ImageManager.setImagenPerfil(imageViewsPuestos[puesto-1], usuario.getAvatar());
+            textViewsPuestos[puesto-1].setText(usuario.getNombre());
+            textViewsPuntos[puesto-1].setText(puntos + " puntos");
+        }else{
+            ImageManager.setImagenPerfil(imageViewsPuestos[puesto-1], ImageManager.IA_IMAGE_ID);
+            textViewsPuestos[puesto-1].setText(PartidaActivity.getIAName());
+            textViewsPuntos[puesto-1].setVisibility(View.GONE);
         }
     }
 
