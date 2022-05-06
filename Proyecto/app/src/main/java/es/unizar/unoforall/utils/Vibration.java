0f1eea6aae4a787b8ebb2b909bbd1f743b7239b7
Iván <ivanpla2001@gmail.com>
2022-05-06 11:56:13 +0200

    Pequeños ajustes internos

--- Proyecto/app/src/main/java/es/unizar/unoforall/utils/Vibration.java
+++ Proyecto/app/src/main/java/es/unizar/unoforall/utils/Vibration.java
@@ -8,12 +8,21 @@ import android.os.Vibrator;
 public class Vibration {
     public static void vibrate(Context context, long milliseconds){
         Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-        // Vibrate for 500 milliseconds
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             v.vibrate(VibrationEffect.createOneShot(milliseconds, VibrationEffect.DEFAULT_AMPLITUDE));
         } else {
             //deprecated in API 26
             v.vibrate(milliseconds);
+        }
+    }
+
+    public static void vibrate(Context context, long... vibrationPattern){
+        Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createWaveform(vibrationPattern, -1));
+        } else {
+            //deprecated in API 26
+            v.vibrate(vibrationPattern, -1);
         }
     }
 }
